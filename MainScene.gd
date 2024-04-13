e10a943f353f1cb8bb48478fa8870cb96e262881
AlxGravel <alexandre.gravel@live.ca>
2024-04-13 15:05:30 -0400

    stuff

--- MainScene.gd
+++ MainScene.gd
@@ -1,9 +1,11 @@
 extends Label
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.use_accumulated_input = false
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- chalk.gd
+++ chalk.gd
@@ -1,11 +1,12 @@
 extends Node
 
+var cursor_speed = 1
 var current_line = Line2D.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.add_child(current_line)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,10 +16,20 @@ func _process(delta):
 
 func _input(event):
 	if event is InputEventMouseMotion:
+		$Cursor.position += event.relative * cursor_speed
+		#confine the cursor to the viewport
+		var visible_rect = get_viewport().get_visible_rect()
+		$Cursor.position = $Cursor.position.clamp(visible_rect.position, visible_rect.end)
+
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			current_line.add_point(event.position)
+			current_line.add_point($Cursor.position)
+
 	elif event is InputEventMouseButton:
 		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			current_line = Line2D.new()
 			self.add_child(current_line)
 
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
