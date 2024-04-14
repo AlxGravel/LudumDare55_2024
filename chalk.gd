a1bff5db48cc9f9f7f1350cb844e424da86d7d3b
manulecrustace <emmanuelboucher@outlook.com>
2024-04-14 00:19:03 -0400

    Tutorial and candles better

--- chalk.gd
+++ chalk.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal point_added(point)
 signal cleared_chalk
+signal left_clicked
 
 @export var cursor_speed = 1
 @export var can_draw = true
@@ -12,6 +13,7 @@ signal cleared_chalk
 func _ready():
 	self.add_child(lines.back())
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$Cursor.position = (Vector2(500,300))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -36,6 +38,7 @@ func _input(event):
 		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			lines.push_back(Line2D.new())
 			self.add_child(lines.back())
+			left_clicked.emit()	
 
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			#clear chalk
@@ -45,6 +48,7 @@ func _input(event):
 			lines.push_back(Line2D.new())
 			self.add_child(lines.back())
 			cleared_chalk.emit()
+			
 
 
 func _on_area_2d_area_entered(area):
