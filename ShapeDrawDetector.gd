6b6c97a1721dcf1f8b9590c3530aaf158bfa8d32
AlxGravel <alexandre.gravel@live.ca>
2024-04-14 00:23:06 -0400

    correct completionrate bug

--- ShapeDrawDetector.gd
+++ ShapeDrawDetector.gd
@@ -5,15 +5,16 @@ signal drew_outside_model
 @export var modelTolerance = 20
 @export var CompletionTolerance = modelTolerance
 
-var drawn_points_cache = Array()
-var completed_points = 0
+var path_points_todo = Array()
 var total_points = 0
 var completionRate = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for path in get_children():
 		if path is Path2D:
-			total_points += path.curve.get_baked_points().size();
+			for p in path.curve.get_baked_points():
+				path_points_todo.push_back(p)
+	total_points = path_points_todo.size()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,7 +25,6 @@ func _process(delta):
 func _on_chalk_point_added(point):
 	#check if the point drawn is outside area
 	update_completion_rate(point)
-	drawn_points_cache.push_back(point)
 	if not is_in_model(point):
 		drew_outside_model.emit()
 
@@ -42,15 +42,17 @@ func is_in_model(point: Vector2) -> bool:
 	return false
 
 func update_completion_rate(point: Vector2) -> float:
-	for path in get_children():
-		if not path is Path2D:
-			continue
+	var q_index = 0
+	while q_index < path_points_todo.size():
+		if point.distance_to(path_points_todo[q_index]) < CompletionTolerance:
+			path_points_todo.remove_at(q_index)
+		q_index += 1
 
-		for q_index in drawn_points_cache.size():
-			for p in path.curve.get_baked_points():
-				if p.distance_to(drawn_points_cache[q_index]) < CompletionTolerance:
-					completed_points += 1
-					drawn_points_cache.remove_at(q_index)
-					break
-	completionRate = float(completed_points) / float(total_points)
+	completionRate = float(total_points - path_points_todo.size()) / float(total_points)
 	return completionRate
+
+func compute_precision() -> float:
+	for path in get_children():
+		if not (path is Path2D):
+			continue
+	return 0
