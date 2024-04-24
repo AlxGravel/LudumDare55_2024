1c092826ebaa13db5b7f37f691e024c929cae48f
AlxGravel <alexandre.gravel@live.ca>
2024-04-24 16:37:58 -0400

    i did something but forgot what it was

--- MainScene.gd
+++ MainScene.gd
@@ -51,7 +51,6 @@ func _on_chalk_left_clicked():
 	if $MainTutorial != null:
 		$MainTutorial.nextTutorialStep()
 		if $MainTutorial.tutorialStep == 1:
-			$Chalk.can_draw = true
 			$PlayerControls.spawn()
 
 
